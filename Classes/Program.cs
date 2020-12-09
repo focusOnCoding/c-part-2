a17428faf54144a40cba7e40636b655a8efbd384
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-09 23:01:46 +0200

    done with classes on to constructors

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,10 +2,31 @@
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0} i am {1}", to , Name);
+        }
+
+        public static Person Parse (string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var person = new Person();
+            var p = Person.Parse("John");
+            person.Name = "Siyabonga";
+            person.Introduce("Dlamini");
             Console.WriteLine("Hello World!");
         }
     }

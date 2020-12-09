3fff7b4d1c23ad1b3113442c69fff6a1cf48e23e
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-10 00:06:51 +0200

    done with constructors and THIS

--- Classes/constructors/Class1.cs
+++ Classes/constructors/Class1.cs
@@ -4,25 +4,30 @@ using System.Text;
 
 namespace constructors
 {
-    public class Customer 
+    public class Customer
     {
         public int Id;
         public string Name;
+        // gineric type <order> is the type of the list
+        public List<order> orders;
 
-        // constructor overload
         public Customer()
         {
-
+            // i should always init to a empty list  
+            orders = new List<order>(); // if i dont do this i get a null error coz this is a referance type
         }
+
+        // constructor overload
         public Customer(int id)
+            : this() // this is how is aad my order list  
         {
             this.Id = id;
         }
 
         public Customer(int id, string name)
+            : this(id) // this means first call the constructor that has the id as a peremiter
         {
-            this.Id = id;
-            this.Name = name; 
+            this.Name = name;
         }
     }
 }

b87baa9b3fe3c25c2349bd7f2ec6185239123265
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-10 00:05:55 +0200

    done with constructors and THIS

---
+++ Classes/constructors/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace constructors
+{
+    public class Customer 
+    {
+        public int Id;
+        public string Name;
+
+        // constructor overload
+        public Customer()
+        {
+
+        }
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name; 
+        }
+    }
+}

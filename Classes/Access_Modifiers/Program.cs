abd5fb5570e549324fcd1c28c4056cee241f9d6f
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-10 15:39:39 +0200

    done with access modifies and the private key

---
+++ Classes/Access_Modifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    public class Person
+    {
+        // hide info
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return _birthdate;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1993, 06, 27));
+            Console.WriteLine(person.GetDateTime());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

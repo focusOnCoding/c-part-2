bed4721782f52887ff027f1b94bfc4d5a3abddf8
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-10 16:17:17 +0200

    a getter e.g

---
+++ Classes/Properties/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Properties
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Birthdate = new DateTime(1993, 06, 27);
+            Console.WriteLine(person.Age);
+        }
+    }
+}

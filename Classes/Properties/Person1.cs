26efe4d2dbbb0b993458b38b8d87c7305b659f0a
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-10 16:27:57 +0200

    done with properties added the Person1 file

---
+++ Classes/Properties/Person1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties
+{
+    public class Person
+    {
+        // create auto impemanted prpertiy {prop    }
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthday)
+        {
+            Birthdate = birthday; 
+        }
+        public int Age
+        {
+            get
+            {
+                var timespane = DateTime.Today - Birthdate;
+                var years = timespane.Days / 365;
+                return years;
+            }
+        }
+    }
+}

1ce7ee4d8b383b1f4e5e43eda9e3835906ec84f7
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-10 10:23:27 +0200

    add global exception

--- Classes/Methods/Program.cs
+++ Classes/Methods/Program.cs
@@ -11,6 +11,20 @@ namespace Methods
             point.Move(new Point(40, 60));
 
             Console.WriteLine("Point is at {0}, {1}", point.X, point.Y  );
+
+            // problem when passing null !!
+            point.Move(null); // theres no VELIDATION 
+
+            // Global excaption handling
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }

d55aeb2e412857caa80ad3cce90a6e6e8ec4089c
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-10 11:25:45 +0200

    done with fields using readonly

---
+++ Classes/fields/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            // add two orders
+            customer.Oreders.Add(new Orders());
+            customer.Oreders.Add(new Orders());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Classes/fields/customer1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fields
+{
+    public class Customer
+    {
+        public int ID;
+        public string Name;
+        // better way so every constructor has access to the list
+        // this field should only be init ones so ill use {readonly}
+        public readonly List<Orders> Oreders = new List<Orders>();
+
+        // Default constructor for list {bad way}
+       /* public Customer()
+        {
+            Order = new List<Orders>(); 
+        }*/
+        public Customer(int id)
+        {
+            this.ID = id;
+        }
+        public Customer(int id, string name)
+            // deliga the code of the id to the to the constructor above
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+        //
+        public void Promote()
+        {
+             
+        }
+    }
+
+    public class Orders
+    {
+
+    }
+}

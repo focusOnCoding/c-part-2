482265d957c16066a925be6df9eebd0acdca881d
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-15 16:48:46 +0200

    Down casting

--- Iheritance_Second_Pillar_of_OOP/UpcastingAndDowncasting/Program.cs
+++ Iheritance_Second_Pillar_of_OOP/UpcastingAndDowncasting/Program.cs
@@ -21,6 +21,14 @@ namespace UpcastingAndDowncasting
             //[Upcasting]
             var anotherList = new List<int>(); // so every item in this list will be an (int)
             var shapes = new List<Shape>(); // list of shapes
+
+            //[downcasting]
+            Shape shape = new Text();
+            // the above gives me a limited view coz its a shape object of type text
+            //[Downcasting] or convert text to shape
+            Text text = (Text)shape;
+            // now i have all access to the text class properties
+
         }
     }
 }

7d54295b01280e9b4e6877a59f301c9a441d796c
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-15 03:52:33 +0200

    done with inheritance on to composition

---
+++ Association_Between_Classes/Inheritance1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Association_Between_Classes
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("object copied to clipbord.");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("object was Duplicated.");
+        }
+    }
+}
---
+++ Association_Between_Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Association_Between_Classes
+{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            // now i have inherited this from the parent class Inheritace1 in this case
+            text.Width = 100;
+            text.Copy();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello {0} {1}");
+        }
+    }
+}

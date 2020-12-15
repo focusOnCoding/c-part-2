8040524e87e0be26621b77f298bb684991c2e7e3
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-15 14:58:59 +0200

    why protected is bad

--- Iheritance_Second_Pillar_of_OOP/Access_Modifiers/Program.cs
+++ Iheritance_Second_Pillar_of_OOP/Access_Modifiers/Program.cs
@@ -2,34 +2,21 @@
 
 namespace Access_Modifiers
 {
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        // incapsulation e.g
-        public void Promote()
-        {
-            // e.g
-            var rating = CalculateRating();
-            if (rating == 0)
-                Console.WriteLine("Promoted to level 1");
-            else
-                Console.WriteLine("Promoted to level 2");
-        }
-
-        public int CalculateRating()
-        {
-            return 0;
-        }
-    }
     class Program
     {
+        public class GoldCustomer : Customer
+        {
+            public void OfferVouchar()
+            {
+                // because of a change this is broken
+                var rating = this.CalculateRating(excluderOrders: true);
+            }
+        }
         static void Main(string[] args)
         {
             var customer = new Customer();
             // becuase of incapsulation i won be abut to do this
-            var rating = customer.CalculateRating();
+            //var rating = customer.CalculateRating();  // now private
             Console.WriteLine("Hello World!");
         }
     }

fb6281a1c1b32c69794240ea561ff5ec66c3d3c8
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-15 04:38:39 +0200

    done with composition on to which is better comp || inher

---
+++ Association_Between_Classes/Composition/DBMigrater1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition
+{
+    public class DBMigrater
+    {
+
+        private readonly Logger _logger;
+
+        // class as parameter in this Composition e.g
+        public DBMigrater(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("We are migrating vlah blah blah ....");
+        }
+    }
+}
---
+++ Association_Between_Classes/Composition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigration = new DBMigrater(new Logger());
+            var logger = new Logger();
+            var installer = new Installer(logger);
+            dbMigration.Migrate();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

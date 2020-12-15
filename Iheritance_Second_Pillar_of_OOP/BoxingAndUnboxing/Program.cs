489eef3e13226fc2d652e74020dd1e1f860a3b7d
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-15 17:20:42 +0200

    done with the tutorial

---
+++ Iheritance_Second_Pillar_of_OOP/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1); // this takes object type so that alaws boxing
+            list.Add("Siya");
+            list.Add(DateTime.Today); // boxing
+
+            // trying to unbox this array[1] will return an error
+            var number = (int)list[1]; // string
+
+            // better way
+            var anotherList = new List<int>();
+            // this is more type safe also no boxing will happen coz this is a list of int and not objects like the listArray above
+            anotherList.Add(1 );
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

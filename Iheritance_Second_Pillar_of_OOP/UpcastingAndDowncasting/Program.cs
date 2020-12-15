d3e4b79d4a226d5949ba59d39b4c2e9041226e5e
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-15 16:26:40 +0200

    done with first example on to the next one

---
+++ Iheritance_Second_Pillar_of_OOP/UpcastingAndDowncasting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UpcastingAndDowncasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text text = new Text();
+            Shape shape = new Text(); // both these two classes point to the same place in memory
+
+            text.Width = 200;
+            shape.Width = 100;
+            Console.WriteLine(text.Width);
+        }
+    }
+}
---
+++ Iheritance_Second_Pillar_of_OOP/UpcastingAndDowncasting/Text.cs
@@ -0,0 +1,8 @@
+﻿namespace UpcastingAndDowncasting
+{
+    public class Text : Shape
+    {
+        public string FontSize { get; set; }
+        public string FontName { get; set; }
+    }
+}

ce4bfabf43e47b08cca78d9373243c4e9fc3b64d
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-16 18:37:26 +0200

    did not learn anything yet coz he was talking about test not at that stage yet

---
+++ Interface/InterfacesAndTestability/OrderProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Testability
+{
+    public class OrderProcessor
+    {
+        // change this to a interface so im not dependent on this class
+        private readonly IShippingCalculator _shippingCalculator;
+
+        public OrderProcessor(IShippingCalculator shippingCalculator)
+        {
+            // change this so i isolate this from ShippingCalculator
+            //_shippingCalculator = new ShippingCalculator();
+            _shippingCalculator = shippingCalculator; // now im just referancing the shC
+        }
+
+        public void Process(Order order)
+        {
+            // defansive programming
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed.");
+
+            // when tesing a class i need to isolat it from other classes
+            // thats why its called unit tseting
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+}
---
+++ Interface/InterfacesAndTestability/ShippingCalculator.cs
@@ -0,0 +1,22 @@
+﻿ namespace Testability
+{
+    // here i define a role of this class
+    // interface methods a always public
+    public interface IShippingCalculator
+    {
+        // no access modifier,
+        float CalculateShipping(Order order); // takes an oder object
+    }
+
+    // interfaces dont use inheritance but they implemant the interface
+    public class ShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            if (order.TotalPrice < 30f)
+                return order.TotalPrice * 0.1f;
+
+            return 0;
+        }
+    }   
+}

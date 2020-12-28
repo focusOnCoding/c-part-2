7c72763050f3ec10cb730b9617da10a637520d47
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-28 11:36:09 +0200

    eg of using .Net delegate System.Func and System.Action

--- ADVANCED_CSHARP/Delegates/PhotoProcessor.cs
+++ ADVANCED_CSHARP/Delegates/PhotoProcessor.cs
@@ -2,16 +2,24 @@
 {
     public class PhotoProcessor
     {
-        public void Proccess(string path)
+        public delegate void PhototFilterHandler(Photo photo);
+      //public void Proccess(string path)
+        public void Process(string path, PhototFilterHandler filterHandler)
         {
+            // use .NET Delegates
+            //System.Action<> {void}
+            //System.Func<> {returns Value}
 
             var photo = Photo.Load(path);
 
-            var filters = new PhotoFilters();
-            filters.ApplyBrightness(photo);
-            filters.ApplyContrast(photo);
-            filters.Resize(photo);
+            //var filters = new PhotoFilters();
+            filterHandler(photo);
 
+            /* filters.ApplyBrightness(photo);
+               filters.ApplyContrast(photo);
+               filters.Resize(photo); */
+
+            filterHandler(photo); // client disgns what he|she wants this method to act  
             photo.Save();
         }
     }
--- ADVANCED_CSHARP/Delegates/Program.cs
+++ ADVANCED_CSHARP/Delegates/Program.cs
@@ -6,7 +6,21 @@ namespace Delegates
     {
         static void Main(string[] args)
         {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            PhotoProcessor.PhototFilterHandler filterHandler = filters.ApplyBrightness;
+
+            
+            // add another filter
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+            processor.Process("phoo.jpg", filterHandler);
             Console.WriteLine("Hello World!");
+        }
+        // if i want to create my own filter that did not come with the framwork i can do the below
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Appy RemoveRedEye");
         }
     }
 }

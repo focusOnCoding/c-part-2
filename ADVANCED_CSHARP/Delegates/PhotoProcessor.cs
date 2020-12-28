b6d863fc14861e94d06e8a066bc0bd63fbfcbcd0
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-28 11:08:17 +0200

    the non extandeble way not using delegates

---
+++ ADVANCED_CSHARP/Delegates/PhotoProcessor.cs
@@ -0,0 +1,18 @@
+﻿namespace Delegates // this is not extandeble but i can make is so using DELEGATES
+{
+    public class PhotoProcessor
+    {
+        public void Proccess(string path)
+        {
+
+            var photo = Photo.Load(path);
+
+            var filters = new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+}

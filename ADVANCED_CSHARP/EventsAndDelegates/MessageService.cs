1848a4b2d83e7ade18b7fe4d71cf1fae57db7d35
Siyabonga Dlamini <focusoncoding@gmail.com>
2021-01-03 08:15:45 +0200

    done with Events and delegates but i would like to write this using less code so im going to twik this code up a bit make it more readeble

---
+++ ADVANCED_CSHARP/EventsAndDelegates/MessageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EventsAndDelegates
+{
+    // responseble for sending text massege
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, videoEventArgs args)
+        {
+            Console.WriteLine("MessageService: Sending a text message..." + args.Video.Title);
+        }
+    }
+}
---
+++ ADVANCED_CSHARP/EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class videoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+    public class VideoEncoder
+    {
+        // 1- Define a delegate
+        // 2- Define an event based on that delegate
+        // 3- Raise the event
+
+        // 1-
+        public delegate void VideoEncodedEventHandler(object source, /*EventArgs args*/ videoEventArgs args);
+        // 2-
+        public event VideoEncodedEventHandler VideoEncoded;
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            // ress the event
+            OnVideoEncoded(video);
+        }
+
+        // 3-
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            // check if i have any subscribies
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, new videoEventArgs() { Video = video });
+            }
+        }
+    }
+}

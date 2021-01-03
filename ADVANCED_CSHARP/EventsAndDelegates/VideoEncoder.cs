ffe3df431462aab37ff25274b20845122cecac0f
Siyabonga Dlamini <focusoncoding@gmail.com>
2021-01-03 08:27:35 +0200

    did some refacterring now y code is a bit mre moden or to the next lesson

--- ADVANCED_CSHARP/EventsAndDelegates/VideoEncoder.cs
+++ ADVANCED_CSHARP/EventsAndDelegates/VideoEncoder.cs
@@ -15,9 +15,15 @@ namespace EventsAndDelegates
         // 3- Raise the event
 
         // 1-
-        public delegate void VideoEncodedEventHandler(object source, /*EventArgs args*/ videoEventArgs args);
+        //public delegate void VideoEncodedEventHandler(object source, /*EventArgs args*/ videoEventArgs args);
+        //[1] now i can remove the above
+
+        // so i dont have to create tyhe above delegate every time i want to create a event handler
+        // EventHandler => i can use this if i dont want to send the event with addtional data
+        // EventHandler<TEventArgs> => use this if there is addtional data i want to send with the event as i have done belowe
+        public event EventHandler<videoEventArgs> VideoEncoded; //[1]
+
         // 2-
-        public event VideoEncodedEventHandler VideoEncoded;
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding Video...");

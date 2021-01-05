9b5579870de9450b93bbf2d391a1eeaf6b43092f
Siyabonga Dlamini <focusoncoding@gmail.com>
2021-01-05 09:10:16 +0200

    formated the code and added Query Linqs

--- ADVANCED_CSHARP/Links/Program.cs
+++ ADVANCED_CSHARP/Links/Program.cs
@@ -25,15 +25,27 @@ namespace Links
 
             var bookLinq = new BookReposisory().GetBook();
             
+            // LINQ Extension Methods
             // i can use where() to filter collections    {{ CHANNING }} Select can be used for transformations or projections 
-            var cheapBookLinks = bookLinq.Where(b => b.Price < 10).OrderBy(b => b.Title).Select(b => b.Title);
+            var cheapBookLinks = bookLinq
+                .Where(b => b.Price < 10)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
 
             foreach (var book in cheapBookLinks)
                 Console.WriteLine(book/*book.Title + " " + book.Price*/);
 
             //BookLinqs.OrderBy(b => b.Title);
-            
+
             // argument => expression === annuonimas function on the fly {Lambder Expression}
+
+            // LINQ Query Operators always start with from and end with Select
+            var cheaperBooks = from b in bookLinq
+                               where b.Price < 10
+                               orderby b.Title
+                               select b.Title;
+            // or return the list of books (b)
+
         }
     }
 }
